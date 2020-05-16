7a17b04f7897a4fa671dd4f433d1921ce39b415d
princeTPG <37287926+princeTPG@users.noreply.github.com>
2020-05-16 23:23:47 +0530

    use shortId module to generate shorrId for urls.

--- src/helpers/shortId.js
+++ src/helpers/shortId.js
@@ -1,6 +1,12 @@
+import shortid from 'shortid';
+
+export const getShortUniqueId = () => {
+    return shortid.generate().toLowerCase();
+}
+
 // generate unique-id from current-timeStamp
 // and convert it to a number-str using toString fucntion and provide a base of 20
-export const getShortUniqueId = () => {
+export const getShortUniqueIdFromDate = () => {
     const currentDate = new Date().getTime();
     return currentDate.toString(20);
 }
